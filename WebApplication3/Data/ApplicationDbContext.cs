1bc6b583a4f295fcf8889795e5f3147656b6cf5d
AlexCodreanu23 <AlexCosmin2377@gmail.com>
2024-01-10 15:13:03 +0200

    Add project files.

---
+++ WebApplication3/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WebApplication3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Game> Games {  get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+    }
+}
---
+++ WebApplication3/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public Game()
+        {
+            
+        }
+
+    }
+}
